cdadd5e8b7fd482c63f9a1f313f4b782eae72a77
chaehaeun <chae_3210@naver.com>
2023-01-10 09:10:13 +0900

    [추가] object

--- client/chapter/core/10-3.function.js
+++ client/chapter/core/10-3.function.js
@@ -1,7 +1,7 @@
 /* ---------------------------------------------------------------------- */
 /* Functions → Arrow                                                      */
 /* ---------------------------------------------------------------------- */
-
+/* 
 const user = {
   sayHi(){
 
@@ -15,7 +15,7 @@ button.addEventListener('click',(e)=>{
   
   e.target.style.background = 'red'
 })
-
+ */
 /* 
 let arr = [1,2,3,4];
 
@@ -25,26 +25,20 @@ function sum(...args){
 }
  */
 
-
 // sum(1,2,3,4,)
 // rest parameter
 const calculateTotal = (...args) => {
-
-  
   let total = 0;
 
-  args.forEach(item=>{
-
-    total += item
-    
-  })
-  
+  args.forEach((item) => {
+    total += item;
+  });
 
   // args.reduce((acc,item)=> acc + item )
   // console.log();
-  
+
   return total;
-}
+};
 
 let resultX = calculateTotal(10000, 8900, 1360, 2100);
 // let resultY = calculateTotal(21500, 3200, 9800, 4700);
@@ -54,20 +48,18 @@ console.log(resultX);
 // console.log(resultY);
 // console.log(resultZ);
 
-
 // 함수 선언 → 화살표 함수 (표현)식
-let calcAllMoney = (a,b,c,d) => a+b+c+d;
-
+let calcAllMoney = (a, b, c, d) => a + b + c + d;
 
 // 화살표 함수와 this
 
-function normalFunction(){
-  console.log('일반함수 : ' ,this);
+function normalFunction() {
+  console.log("일반함수 : ", this);
 }
 
-const arrowFunction = ()=>{
-  console.log('화살표함수 : ' ,this);
-}
+const arrowFunction = () => {
+  console.log("화살표함수 : ", this);
+};
 
 // normalFunction()
 // arrowFunction()
@@ -76,61 +68,65 @@ const arrowFunction = ()=>{
 //  메서드 안에서의 함수는 this를 찾기 위해서는 arrowFunction이 오히려 좋아.
 
 const user = {
-  name:'tiger',
-  age:32,
-  address:'서울시 중랑구 면목동',
-  grades:[80,90,100],
-  totalGrades: function (){
-   /*  let total = 0;
+  name: "tiger",
+  age: 32,
+  address: "서울시 중랑구 면목동",
+  grades: [80, 90, 100],
+  totalGrades: function () {
+    /*  let total = 0;
     this.grades.forEach((item)=>{
       this
     })
     return total */
 
-    function foo(){
-      console.log('foo : ', this);
+    function foo() {
+      console.log("foo : ", this);
     }
 
-    const bar = () =>{
-      console.log('bar : ', this);
-    }
+    const bar = () => {
+      console.log("bar : ", this);
+    };
 
-    foo.call(user) // user
-    bar() // user object
-
-  }
-}
-
+    foo.call(user); // user
+    bar(); // user object
+  },
+};
 
 console.log(user.totalGrades());
-
 
 /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
 
 // pow(numeric: number, powerCount: number): number;
-let pow; 
+let pow = (numeric, powerCount) => {
+  let result = 1;
+  for (let i = 0; i < powerCount; i++) {
+    result *= numeric;
+  }
+  return result;
+};
 
+// acc *= numeric
+// acc += text
 
+let powExpression = (numeric, powerCount) =>
+  Array(powerCount)
+    .fill(null)
+    .reduce((acc) => acc * numeric, 1);
 
-pow(2,53) // 9007199254740992
-
+// pow(2,53) // 9007199254740992
 
 // repeat(text: string, repeatCount: number): string;
-let repeat; 
+let repeat = (text, repeatCount) => {
+  let result = "";
+  for (let i = 0; i < repeatCount; i++) {
+    result += text;
+  }
+  return result;
+};
 
+let repeatExpression = (text, repeatCount) =>
+  Array(repeatCount)
+    .fill(null)
+    .reduce((acc) => acc + text, "");
 
-repeat('hello😘',3) //  'hello😘hello😘hello😘'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// repeat('hello😘',3) //  'hello😘hello😘hello😘'
