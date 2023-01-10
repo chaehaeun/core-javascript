225ecbc06bd65f4f39033e48f1ccfd7334e34e3f
chaehaeun <chae_3210@naver.com>
2023-01-10 13:04:08 +0900

    [추가] attr 학습

--- client/chapter/core/10-3.function.js
+++ client/chapter/core/10-3.function.js
@@ -25,20 +25,26 @@ function sum(...args){
 }
  */
 
+
 // sum(1,2,3,4,)
 // rest parameter
 const calculateTotal = (...args) => {
+
+  
   let total = 0;
 
-  args.forEach((item) => {
-    total += item;
-  });
+  args.forEach(item=>{
+
+    total += item
+    
+  })
+  
 
   // args.reduce((acc,item)=> acc + item )
   // console.log();
-
+  
   return total;
-};
+}
 
 let resultX = calculateTotal(10000, 8900, 1360, 2100);
 // let resultY = calculateTotal(21500, 3200, 9800, 4700);
@@ -48,18 +54,20 @@ console.log(resultX);
 // console.log(resultY);
 // console.log(resultZ);
 
+
 // 함수 선언 → 화살표 함수 (표현)식
-let calcAllMoney = (a, b, c, d) => a + b + c + d;
+let calcAllMoney = (a,b,c,d) => a+b+c+d;
+
 
 // 화살표 함수와 this
 
-function normalFunction() {
-  console.log("일반함수 : ", this);
+function normalFunction(){
+  console.log('일반함수 : ' ,this);
 }
 
-const arrowFunction = () => {
-  console.log("화살표함수 : ", this);
-};
+const arrowFunction = ()=>{
+  console.log('화살표함수 : ' ,this);
+}
 
 // normalFunction()
 // arrowFunction()
@@ -68,65 +76,85 @@ const arrowFunction = () => {
 //  메서드 안에서의 함수는 this를 찾기 위해서는 arrowFunction이 오히려 좋아.
 
 const user = {
-  name: "tiger",
-  age: 32,
-  address: "서울시 중랑구 면목동",
-  grades: [80, 90, 100],
-  totalGrades: function () {
-    /*  let total = 0;
+  name:'tiger',
+  age:32,
+  address:'서울시 중랑구 면목동',
+  grades:[80,90,100],
+  totalGrades: function (){
+   /*  let total = 0;
     this.grades.forEach((item)=>{
       this
     })
     return total */
 
-    function foo() {
-      console.log("foo : ", this);
+    function foo(){
+      console.log('foo : ', this);
     }
 
-    const bar = () => {
-      console.log("bar : ", this);
-    };
+    const bar = () =>{
+      console.log('bar : ', this);
+    }
 
-    foo.call(user); // user
-    bar(); // user object
-  },
-};
+    foo.call(user) // user
+    bar() // user object
+
+  }
+}
+
 
 console.log(user.totalGrades());
+
 
 /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
 
 // pow(numeric: number, powerCount: number): number;
 let pow = (numeric, powerCount) => {
-  let result = 1;
-  for (let i = 0; i < powerCount; i++) {
-    result *= numeric;
-  }
-  return result;
+    let result = 1;
+    for(let i = 0; i < powerCount; i++){
+      result *= numeric
+    }
+    return result;
 };
 
-// acc *= numeric
-// acc += text
+                                                                                    // acc *= numeric
+                                                                                    // acc += text
 
-let powExpression = (numeric, powerCount) =>
-  Array(powerCount)
-    .fill(null)
-    .reduce((acc) => acc * numeric, 1);
+let powExpression = (numeric, powerCount) => Array(powerCount).fill(null).reduce(acc=>acc * numeric,1)
+
+
 
 // pow(2,53) // 9007199254740992
 
+
+
 // repeat(text: string, repeatCount: number): string;
 let repeat = (text, repeatCount) => {
-  let result = "";
-  for (let i = 0; i < repeatCount; i++) {
+  let result = '';
+  for(let i = 0; i < repeatCount; i++){
     result += text;
   }
   return result;
-};
+}; 
 
-let repeatExpression = (text, repeatCount) =>
-  Array(repeatCount)
-    .fill(null)
-    .reduce((acc) => acc + text, "");
+
+
+let repeatExpression = (text, repeatCount)=> Array(repeatCount).fill(null).reduce(acc=> acc + text,'')
+
 
 // repeat('hello😘',3) //  'hello😘hello😘hello😘'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/chapter/core/11-2.object.js
+++ client/chapter/core/11-2.object.js
@@ -2,18 +2,19 @@
 /* Copy object by reference                                               */
 /* ---------------------------------------------------------------------- */
 
+
 // 복사(copy) vs. 참조(reference)
 
-let message = "문자 값은 프리미티브 데이터 타입으로 값이 복사됩니다.";
+let message = '문자 값은 프리미티브 데이터 타입으로 값이 복사됩니다.';
 let messenger = {
-  name: "kakao talk",
-  manufacture: "kakao",
+  name: 'kakao talk',
+  manufacture: 'kakao'
 };
 
 let text = message;
 let conversationTool = messenger;
 
-text = "안녕?ㅋ";
+text = '안녕?ㅋ'
 
 // conversationTool.name = 'line'
 
@@ -23,21 +24,25 @@ console.log(message === text);
 console.log(messenger == conversationTool);
 console.log(messenger === conversationTool);
 
+
 // 객체 복사
 // 1. for ~ in 문을 사용한 복사
-const cloneObject = {};
+const cloneObject = {}
 
-for (const key in messenger) {
-  cloneObject[key] = messenger[key];
+for(const key in messenger){
+  cloneObject[key] = messenger[key]
 }
 
 // 2. Object.assign()을 사용한 복사
-const copyObject = Object.assign({}, messenger);
+const copyObject = Object.assign({},messenger)
+
 
 // 3. 전개 연산자(...)를 사용한 복사
-const spreadObject = { ...messenger }; // 진짜 좋아요 🐶꿀
+const spreadObject = {...messenger}  // 진짜 좋아요 🐶꿀 
 
-// 4. 객체를 복사해주는 유틸 함수
+
+// 4. 객체를 복사해주는 유틸 함수 
+
 
 // function copydObject(object){
 //   return Object.assign({},object)
@@ -45,44 +50,54 @@ const spreadObject = { ...messenger }; // 진짜 좋아요 🐶꿀
 
 // const copydObject = (object) => {Object.assign({},object)}
 
-const copydObject = (object) => Object.assign({}, object);
+
+const copydObject = object => Object.assign({},object)
+
+
 
 // 객체 병합(합성)
 const cssMapA = {
-  color: "#4b004b",
-  margin: "0 auto",
+  color: '#4b004b',
+  margin: '0 auto',
 };
 
 const cssMapB = {
-  display: "flex",
-  flexFlow: "column",
-  justifyContent: "center",
-  padding: "0.4em 0.62em",
-  color: "#3f9e97",
+  display: 'flex',
+  flexFlow: 'column',
+  justifyContent: 'center',
+  padding: '0.4em 0.62em',
+  color: '#3f9e97',
 };
 
 // let combinedCssMap = Object.assign({},cssMapA,cssMapB);
-let combinedCssMap = { ...cssMapA, ...cssMapB };
+let combinedCssMap = {...cssMapA,...cssMapB}
+
 
 // 중첩된 프로퍼티에 객체를 포함하는 객체 복사
 // 얕은 복사 vs. 깊은 복사
 const containerStyles = {
-  "min-height": "100vh",
-  "max-width": {
-    sm: "90%",
+  'min-height': '100vh',
+  'max-width': {
+    sm: '90%',
     md: 640,
     lg: 960,
     xl: 1120,
-    xxl: 1140,
+    xxl: 1140
   },
 };
+
+
+
+
+
+
 
 // 1. 깊은 복사 유틸리티 함수
 function cloneDeep(object) {
   return Object.fromEntries(
     Object.entries(object).map(([key, value]) => {
       let type = typeof value;
-      if (value && type === "object") {
+      if (value && type === 'object') {
         value = cloneDeep(value);
       }
       return [key, value];
@@ -90,7 +105,15 @@ function cloneDeep(object) {
   );
 }
 
+
+
+
+
 let copyedContainerStyles = cloneDeep(containerStyles);
+
+
+
+
 
 // 2. Lodash 라이브러리 활용
 // _.cloneDeep(value)
