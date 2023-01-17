5146afb619b33e2278650ea4a96b9e3125d008d9
chaehaeun <chae_3210@naver.com>
2023-01-17 09:14:27 +0900

    [추가] 메모이제이션 패턴

--- client/main.js
+++ client/main.js
@@ -1,106 +1,88 @@
-
-
-import { 
-  diceAnimation, 
-  disableElement, 
-  enableElement, 
-  getNode, 
+import {
+  diceAnimation,
+  disableElement,
+  enableElement,
+  getNode,
   getNodes,
   visibleElement,
-  invisibleElement
- } from "./lib/index.js";
-
+  invisibleElement,
+  insertLast,
+} from "./lib/index.js";
 
 // [ 주사위 굴리기 ]
 // 1. dice 애니메이션 불러오기
-// 2. bindEvent 유틸 함수 만들기 
-// 3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기 
-// 4. 변수 보호를 위한 클로저 + IIFE 사용하기 
+// 2. bindEvent 유틸 함수 만들기
+// 3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기
+// 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
 // [ 레코드 리스트 보이기 ]
 // 1. handleRecord 함수를 만들기
 // 2. disable 활성 유틸 함수 만들기
 // 3. handleReset 함수를 만듭니다.
 // 4. visible 활성 유틸 함수 만들기
-// 5. toggleState 유틸 함수 만들기 
+// 5. toggleState 유틸 함수 만들기
 
 // [ 레코드 템플릿 뿌리기 ]
 // 1. renderRecordListItem 함수 만들기
 // 2. HTML 템플릿 만들기
-// 3. 템플릿 뿌리기 
+// 3. 템플릿 뿌리기
 
+// 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes(
+  ".buttonGroup > button"
+);
 
+const recordListWrapper = getNode(".recordListWrapper");
 
-// 배열의 구조 분해 할당 
-const [rollingDiceButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+function renderRecordListItem() {
+  let template = /*HTML*/ `
+    <tr>
+      <td>${}</td>
+      <td>${}</td>
+      <td>${}</td>
+    </tr>`;
 
-const recordListWrapper = getNode('.recordListWrapper')
-
-
-
-
-function renderRecordListItem(){
-  
+  insertLast(".recordListWrapper tbody", template);
 }
-
-
-
-
 
 /* -------------------------------------------------------------------------- */
 /* event                                                                      */
 /* -------------------------------------------------------------------------- */
 
 const handleRollingDice = (() => {
- 
   let isRolling = false;
-  let stopAnimation;  
+  let stopAnimation;
 
   return () => {
-    if(!isRolling){
+    if (!isRolling) {
       // console.log('첫번째 클릭');
-      stopAnimation = setInterval(diceAnimation,100)
-      
-      disableElement(recordButton)
-      disableElement(resetButton)
-      
-      
-    }else{
+      stopAnimation = setInterval(diceAnimation, 100);
+
+      disableElement(recordButton);
+      disableElement(resetButton);
+    } else {
       // console.log('두번째 클릭');
       clearInterval(stopAnimation);
-      enableElement(recordButton)
-      enableElement(resetButton)
+      enableElement(recordButton);
+      enableElement(resetButton);
     }
-  
+
     isRolling = !isRolling;
-  }
+  };
+})();
 
-})()
-
-
-const handleRecord =()=>{
-  
+const handleRecord = () => {
   visibleElement(recordListWrapper);
 
   renderRecordListItem();
-}
+};
 
 const handleReset = () => {
+  invisibleElement(recordListWrapper);
+};
 
-  invisibleElement(recordListWrapper)
-}
-
-
-
-rollingDiceButton.addEventListener('click',handleRollingDice)
-recordButton.addEventListener('click',handleRecord)
-resetButton.addEventListener('click',handleReset)
+rollingDiceButton.addEventListener("click", handleRollingDice);
+recordButton.addEventListener("click", handleRecord);
+resetButton.addEventListener("click", handleReset);
 
 // let eventOff = bindEvent(rollingDiceButton,'click',handlerRollingDice);
-
-
-
-
-
-
-
